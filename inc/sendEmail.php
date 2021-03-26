cd14a39c0ff0562c951589d6581b6e0e95a9f9b9
ScottyS06 <scottysingh7@gmail.com>
2021-03-26 13:07:38 -0700

    emailing test1

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,5 +1,9 @@
 ﻿<?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Replace this with your own email address
 $siteOwnersEmail = 'scottysingh7@gmail.com';
 
